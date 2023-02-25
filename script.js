d6f5c1914ee91b037dd1954c106125c52f1da3c2
Oleksandr <allekssandr13@gmail.com>
2023-02-25 23:36:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let number_1 = prompt('Enter first number: ');
+let number_2 = prompt('Enter second number: ');
+
+let sum = Number(number_1) + Number(number_2);
+let subtraction = Number(number_1) - Number(number_2);
+let multiplication = Number(number_1) * Number(number_2);
+let division = Number(number_1) / Number(number_2);
+
+alert('The sum of your numbers is: ' + sum + '; the subtraction of your numbers is: ' + subtraction + '; the multiplication of your numbers is: ' + multiplication + '; the division of your numbers is: ' + division);
