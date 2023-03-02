d620758edadffc8506d7932b53a436d068e86db1
AlexShandyga <allekssandr13@gmail.com>
2023-03-02 22:30:53 +0200

    not completed

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let number_1 = prompt('Enter first number: ');
 let number_2 = prompt('Enter second number: ');
 
+if(number_1 < number_2) {
+    alert('Do you want to continue?');
+} 
+
 let sum = Number(number_1) + Number(number_2);
 let subtraction = Number(number_1) - Number(number_2);
 let multiplication = Number(number_1) * Number(number_2);
-let division = Number(number_1) / Number(number_2);
+let division;
+ if (Number(number_2) === 0){
+    division = 'Error';
+    
+} else division = Number(number_1) / Number(number_2);
+
 
 alert('The sum of your numbers is: ' + sum + '; the subtraction of your numbers is: ' + subtraction + '; the multiplication of your numbers is: ' + multiplication + '; the division of your numbers is: ' + division);
