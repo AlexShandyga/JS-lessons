dc9b962ffbf44c17ead35b56e1e9af41217eef84
AlexShandyga <allekssandr13@gmail.com>
2023-03-16 10:59:29 +0200

    final

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 let number_1 = prompt('Enter first number: ');
 let number_2 = prompt('Enter second number: ');
 
-if(number_1 < number_2) {
-    alert('Do you want to continue?');
+if (number_1 === "" || number_2 === "") {
+  alert("You did not entered one of the numbers");
+} else if (number_2 == 0) {
+  alert("Error: you can't divide by 0");
+} else if (number_1 < number_2) {
+  let confirmMessage = confirm(
+    "Do you want to continue?"
+  );
+  if (confirmMessage) {
+    let subtraction = number_1 - number_2;
+    alert(`the subtraction of your numbers is ${subtraction}`);
+  }
 } 
-
 let sum = Number(number_1) + Number(number_2);
-let subtraction = Number(number_1) - Number(number_2);
+alert(`The sum of your numbers is: ${sum}`);
+
 let multiplication = Number(number_1) * Number(number_2);
-let division;
- if (Number(number_2) === 0){
-    division = 'Error';
-    
-} else division = Number(number_1) / Number(number_2);
+alert(`The multiplication of your numbers is: ${multiplication}`);
+
+let division = Number(number_1) / Number(number_2);
+alert(`The division of your numbers is: ${division}`);
 
 
-alert('The sum of your numbers is: ' + sum + '; the subtraction of your numbers is: ' + subtraction + '; the multiplication of your numbers is: ' + multiplication + '; the division of your numbers is: ' + division);
+
+
