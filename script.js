b171bf37556d712f864c0379289ff975c6d70d76
Oleksandr <allekssandr13@gmail.com>
2023-03-27 18:34:19 +0300

    lesson-26 complete

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-function isObjectEmpty(obj) {
-    if (Object.keys(obj).length === 0) {
-        console.log('Object is empty');
-    }
-    else return 'Object is not empty'
-};
+function Accumulator(number) {
+    this.number = number;
+    this.increment = function() {
+      this.number++;
+      return this.number;
+    };
+    this.decrement = function() {
+      this.number--;
+      return this.number;
+    };
+  };
 
-const user = {
-    name: 'Alex',
-    age: 33,
-    sayHello() {
-        console.log(`Hello, my name is ${this.name}, I'm ${this.age} years old.`)
-    }
-};
+    function CancelableAccumulator(number) {
+    Accumulator.call(this, number);
+  
+    this.clear = function() {
+      this.number = number;
+    };
+  }
+CancelableAccumulator.prototype = Object.create(Accumulator.prototype);
+CancelableAccumulator.prototype.constructor = CancelableAccumulator;
 
+let acc = new CancelableAccumulator(4);
 
-
-let Calculator = {
-    num1: 0,
-    num2: 0,
-    ask() {
-        this.num1 = +prompt('Enter first digit: ');
-        this.num2 = +prompt('Enter second digit: ');
-    },
-    sum() {
-        return this.num1 + this.num2;
-    },
-    mul() {
-        return this.num1 * this.num2;
-    }
-};
+console.log(acc.increment());
+console.log(acc.decrement());
+console.log(acc.decrement());
+acc.clear()
+console.log(acc.number);
