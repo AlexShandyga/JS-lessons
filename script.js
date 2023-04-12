fe16273aabe8cde74c4388fc608107e42d822487
Oleksandr <allekssandr13@gmail.com>
2023-04-12 11:10:14 +0300

    in progress

--- script.js
+++ script.js
@@ -32,5 +32,36 @@ for (let i = 1; i <= rowLength; i++) {
     console.log(pattern);
 }
 
+// homework 26
 
+function Accumulator(number) {
+    this.number = number;
+    this.increment = function () {
+        this.number++;
+        return this.number;
+    };
+    this.decrement = function () {
+        this.number--;
+        return this.number;
+    };
+};
 
+function CancelableAccumulator(number) {
+    Accumulator.call(this, number);
+
+    this.clear = function () {
+        this.number = number;
+    };
+}
+CancelableAccumulator.prototype = Object.create(Accumulator.prototype);
+CancelableAccumulator.prototype.constructor = CancelableAccumulator;
+
+let acc = new CancelableAccumulator(4);
+
+console.log(acc.increment());
+console.log(acc.decrement());
+console.log(acc.decrement());
+acc.clear()
+console.log(acc.number);
+
+// homework 28
